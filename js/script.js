3402a9a333d8bbeb35e5fa8b5e3c9584d68aa218
Nerea García <nereagarcia.c@gmail.com>
2021-02-23 21:15:14 +0100

    creación funcion esPalindromo

---
+++ js/script.js
@@ -0,0 +1,5 @@
+let noPalindromo = "Crepes para todos!";
+let siPalindromo = "Así Ramona va, no Marisa.";
+
+console.log(esPalindromo(noPalindromo), `"${noPalindromo}" no es un Palíndromo`);
+console.log(esPalindromo(siPalindromo), `"${siPalindromo}" sí es un Palíndromo`);
